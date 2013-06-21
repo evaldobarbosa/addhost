460aad3ddf9f115eb1a289a85313120c0a55f804
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-06-21 11:57:40 -0300

    Update addhost.php
    --composer option added

--- addhost.php
+++ addhost.php
@@ -6,8 +6,9 @@ echo "CONFIGURADOR DE HOSTS APACHE\n";
 echo "by: EVALDO BARBOSA\n";
 
 $htaccess = ( in_array( '--htaccess', $argv) );
+$composer = ( in_array( '--composer', $argv) );
 
-$addhost = new AddHost( $argv[1], $argv[2], $argv[3],$htaccess);
+$addhost = new AddHost( $argv[1], $argv[2], $argv[3],$htaccess,$composer);
 
 $log = $addhost->run();
 
