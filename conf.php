d1868d843560d2461f2a18a20acdb2c155538e18
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-06-21 11:10:59 -0300

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,8 @@
+<?
+ini_set("display_errors", "Off");
+ini_set("error_reporting",E_ALL ^ E_NOTICE ^ E_WARNING);
+
+define("APACHE_VHOST_PATH","/etc/apache2/sites-enabled");
+define("APACHE_GROUP","www-data");
+define("CURRENT_USER","evaldo");
+define("HOSTS_FILE","/etc/hosts");
