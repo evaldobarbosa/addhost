f1d51fc02378cf721719f03d3669e9c6afe8ef65
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-08-07 11:38:21 -0300

    Update conf.php

--- conf.php
+++ conf.php
@@ -6,3 +6,6 @@ define("APACHE_VHOST_PATH","/etc/apache2/sites-enabled");
 define("APACHE_GROUP","www-data");
 define("CURRENT_USER","evaldo");
 define("HOSTS_FILE","/etc/hosts");
+
+//define("PROXY_HOST",'proxy.host.com.br:3128');
+//define("PROXY_USER",'evaldobarbosa:myproxypassword');
