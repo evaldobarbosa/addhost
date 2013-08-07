ac88f486f2075d44a1017ce341388b3a53d1fd67
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-08-07 15:35:41 -0300

    Create pt_br.lang.php

---
+++ pt_br.lang.php
@@ -0,0 +1,37 @@
+<?
+return array(
+  'project_name'				=> 'CONFIGURADOR DE HOSTS APACHE',
+	'success'					=> 'SUCESSO',
+	'error'						=> 'ERRO',
+	'created_by'				=> 'CREATED BY',
+	'copy_file'					=> 'ARQUIVOS COPIADOS',
+	'param_first_error'			=> 'O primeiro parâmetro deve ser o IP da aplicação a ser configurado',
+
+	'vhost_label'				=> 'CONFIGURANDO VIRTUALHOST',
+	'vhost_exists'				=> 'Virtualhost já configurado',
+	'vhost_create_error'		=> 'Erro ao criar arquivo de vhost',
+	'vhost_remove_success'		=> 'Virtual host removido',
+	'vhost_remove_error'		=> 'Erro ao remover arquivo de virtual host',
+	'vhost_not_found'			=> 'Arquivo de virtual host não encontrado',
+
+	'host_label'				=> 'CONFIGURANDO HOST',
+	'host_add_name_error'		=> 'Erro ao adicionar host no arquivo',
+	'host_success'				=> 'HOSTNAME CONFIGURADO',
+	'host_remove_ip_error'		=> 'Host ou IP não encontrados',
+
+	'folder_create_error'		=> 'Erro ao criar pastas do host',
+	'folder_removed'			=> 'Host removido',
+	'folder_not_found'			=> 'Pasta do projeto não encontrada',
+	'folder_remove_error'		=> 'Erro ao remover a pasta do projeto',
+	'folder_remove_manually'	=> 'Remova a pasta manualmente',
+
+	'htaccess_label'			=> 'CONFIGURANDO HTACCESS',
+	'htaccess_create_error'		=> 'Erro ao criar htaccess',
+	'htaccess_success'			=> 'SEU HTACCESS FOI CRIADO CORRETAMENTE',
+
+	'composer_label'			=> 'DOWNLOAD DO COMPOSER',
+	'composer_dl_error'			=> 'Erro no download do composer',
+	'composer_success'			=> 'DOWNLOAD DO COMPOSER REALIZADO',
+	'composer_json_success'		=> 'CRIANDO ARQUIVO composer.json PADRÃO',
+	'composer_json_error'		=> 'ERRO AO CRIAR composer.json'
+);
