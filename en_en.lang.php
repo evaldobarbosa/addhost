2c7d1fe13f4d60a29186d0f670e71f47950acebf
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-08-16 11:26:42 -0300

    Create en_en.lang.php

---
+++ en_en.lang.php
@@ -0,0 +1,53 @@
+<?
+/********************************************************************************************************
+*
+* Languagefile for
+* PHP Add Apache Virtual Host (class)
+* by Evaldo Barbosa
+*
+* @source = http://www.phpclasses.org/package/8114-PHP-Add-a-virtual-host-to-Apache-configuration.html
+*
+* ENGLISH TRANSLATION BY
+*
+* @name = Evaldo Barbosa
+* @mail = evaldobarbosa@gmail.com
+*
+********************************************************************************************************/
+return array(
+	'project_name'				=> 'Apache\'s Virtual Hosts Creator',
+	'success'					=> 'SUCCESS',
+	'error'						=> 'ERROR',
+	'alert'						=> 'WARNING',
+	'created_by'				=> 'CREATED BY',
+	'copy_file'					=> 'Arquivos copiados',
+	'param_first_error'			=> 'The first parameter should be the IP address to be configured',
+
+	'vhost_label'				=> 'SETTING VIRTUALHOST',
+	'vhost_exists'				=> 'Virtual host already setted',
+	'vhost_create_error'		=> 'Error while creating the vhost file',
+	'vhost_remove_success'		=> 'Virtual host removed',
+	'vhost_remove_error'		=> 'Error while removing virtual host file',
+	'vhost_not_found'			=> 'Virtual host not found',
+
+	'host_label'				=> 'SETTING HOST NAME',
+	'host_add_name_error'		=> 'Error while adding host name',
+	'host_success'				=> 'HOSTNAME SETTED',
+	'host_remove_ip_error'		=> 'Host or IP not found',
+
+	'folder_create_error'		=> 'Error while creating project folder',
+	'folder_removed'			=> 'Host removed',
+	'folder_not_found'			=> 'Pasta do projeto não encontrada',
+	'folder_remove_error'		=> 'Error while removing project follder',
+	'folder_remove_manually'	=> 'Remove the project folder manually',
+
+	'htaccess_label'			=> 'SETTING HTACCESS',
+	'htaccess_create_error'		=> 'Error while creating htaccess',
+	'htaccess_success'			=> 'YOUR HTACCESS WAS CREATED SUCCESSFULLY',
+
+	'composer_label'			=> 'COMPOSER DOWNLOAD',
+	'composer_dl_error'			=> 'Error while downloading composer',
+	'composer_success'			=> 'COMPOSER DOWNLOADED SUCCESSFULLY',
+	'composer_json_success'		=> 'SETTING composer.json DEFAULT FILE',
+	'composer_json_error'		=> 'Error while creating composer.json',
+	'composer_licence_warn'		=> 'Review the license within composer.json'
+);
