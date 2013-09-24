9f7235cbd87c20f794fd154f0a0c63739555fd0f
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-09-24 11:42:42 -0300

    Update conf.php
    Adding DEFAULT_PRJ_PATH

--- conf.php
+++ conf.php
@@ -6,6 +6,7 @@ define("APACHE_VHOST_PATH","/etc/apache2/sites-enabled");
 define("APACHE_GROUP","www-data");
 define("CURRENT_USER","evaldo");
 define("HOSTS_FILE","/etc/hosts");
+define("DEFAULT_PRJ_PATH", $_SERVER['HOME'] . "/Projetos");
 
 //define("PROXY_HOST",'proxy.host.com.br:3128');
 //define("PROXY_USER",'evaldobarbosa:myproxypassword');
